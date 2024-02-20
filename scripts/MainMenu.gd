8b196a731aac4a19bf048b95b5270af11e838928
Filip Tutić <ftutic@vub.hr>
2024-02-20 14:40:53 +0100

    Actually add the menu

---
+++ scripts/MainMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://scenes/TestScene.tscn")
+
+
+func _on_info_pressed():
+	pass # Replace with function body.
+
+
+func _on_exit_pressed():
+	get_tree().quit()
