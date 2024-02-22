82d20efabf31c545ce12629fb940671b87cd90d1
Filip Tutić <ftutic@vub.hr>
2024-02-22 17:48:30 +0100

    attacks

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,18 +5,34 @@ const SPEED = 300.0
 const ROLL_SPEED = 280.0
 const LOCK_SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+const SLAM_VELOCITY = 800.0
+const ATTACK_VELOCITY = 100.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-# Exists so idle animation doesnt play over the jump animation
-var jumped = false
+# Exists so idle animation doesnt play over animations
+var pause_idle = false
+var pause_movement=false
+var slammed=false
 
 #active rolling animation
 @export var rollingAnimActiveFrame: int = 0
 
 var headCrushed = false
+var fallTimer = 0.4
 
+func _ready():
+	rollingAnimActiveFrame = 0
+	$Marker2D/Sprite2D/AnimationPlayer.play("RESET")
+func _process(delta):
+	if not is_on_floor():
+		fallTimer -= delta
+	else:
+		fallTimer = 0.4
+	if fallTimer<0 and !pause_movement:
+		$Marker2D/Sprite2D/AnimationPlayer.play("jump_midair")
+		pause_idle = false
 
 func _sprite_direction_flip(direction):
 	if direction == 1:
@@ -28,33 +44,41 @@ func _physics_process(delta):
 	var direction = $Marker2D.scale.x
 	var inputAxis = Input.get_axis("move-left", "move-right")
 	var locked = Input.is_action_pressed("lock")
-	if !locked:
+	slammed=false
+	if !locked and inputAxis != 0:
 		direction = inputAxis
-	jumped=false
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 		
+	#needs to be a one and done
+	if Input.is_action_just_pressed("jump") and !rollingAnimActiveFrame:
+		#Roll
+		if locked and is_on_floor():
+			rollingAnimActiveFrame = 1
+			pause_movement=true
+			$Marker2D/Sprite2D/AnimationPlayer.play("roll")
+			if inputAxis == 0:
+				inputAxis = direction
+			velocity.x = inputAxis * ROLL_SPEED
+			pause_idle=true
+			
+		#Jump
+		elif is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			$Marker2D/Sprite2D/AnimationPlayer.play("jump")
+			pause_idle=true
 	
-		
-	if Input.is_action_just_pressed("jump") and locked and is_on_floor() and !rollingAnimActiveFrame:
-		rollingAnimActiveFrame = 1
-		$Marker2D/Sprite2D/AnimationPlayer.play("roll")
-		if inputAxis == 0:
-			inputAxis = direction
-		velocity.x = inputAxis * ROLL_SPEED
-		
-	# Handle jump.
-	elif Input.is_action_just_pressed("jump") and is_on_floor() and !rollingAnimActiveFrame:
-		velocity.y = JUMP_VELOCITY
-		$Marker2D/Sprite2D/AnimationPlayer.play("jump")
-		$Marker2D/Sprite2D/AnimationPlayer.queue("jump_midair")
-		jumped=true
-		
-
+	if Input.is_action_pressed("attack-1") and Input.is_action_pressed("jump") and locked and !rollingAnimActiveFrame and not is_on_floor():
+		velocity.y = SLAM_VELOCITY
+		$Marker2D/Sprite2D/AnimationPlayer.stop()
+		$Marker2D/Sprite2D/AnimationPlayer.play("slam")
+		pause_movement=false
+		slammed=true
 	if rollingAnimActiveFrame:
 		if rollingAnimActiveFrame == 6:
 			rollingAnimActiveFrame = 0
+			pause_movement=false
 		elif rollingAnimActiveFrame == 5:
 			if inputAxis:
 				velocity.x = inputAxis * LOCK_SPEED
@@ -66,8 +90,25 @@ func _physics_process(delta):
 		velocity.x = inputAxis * ROLL_SPEED
 		rollingAnimActiveFrame = 1
 		$Marker2D/Sprite2D/AnimationPlayer.play("roll")
-	elif inputAxis:
-		
+	elif Input.is_action_just_pressed("attack-1") and !pause_movement and !slammed:
+		inputAxis = direction
+		velocity.x = inputAxis * ATTACK_VELOCITY
+		$Marker2D/Sprite2D/AnimationPlayer.play("attack-1")
+		pause_idle=true
+		pause_movement=true
+	elif Input.is_action_just_pressed("attack-2") and !pause_movement:
+		inputAxis = direction
+		velocity.x = inputAxis * ATTACK_VELOCITY
+		$Marker2D/Sprite2D/AnimationPlayer.play("attack-2")
+		pause_idle=true
+		pause_movement=true
+	elif Input.is_action_just_pressed("attack-3") and !pause_movement:
+		inputAxis = direction
+		velocity.x = inputAxis * ATTACK_VELOCITY
+		$Marker2D/Sprite2D/AnimationPlayer.play("attack-3")
+		pause_idle=true
+		pause_movement=true
+	elif inputAxis and !pause_movement:
 		var anim = "walk"
 		if locked:
 			velocity.x = inputAxis * LOCK_SPEED
@@ -78,11 +119,11 @@ func _physics_process(delta):
 		else:
 			velocity.x = inputAxis * SPEED
 			_sprite_direction_flip(inputAxis)
-		if is_on_floor() and jumped==false:
+		if is_on_floor() and !pause_idle:
 			$Marker2D/Sprite2D/AnimationPlayer.play(anim)
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		if is_on_floor() and jumped==false:
+		if is_on_floor() and !pause_idle:
 			$Marker2D/Sprite2D/AnimationPlayer.play("idle")
 
 	move_and_slide()
@@ -102,3 +143,5 @@ func _on_roll_failure_detector_body_entered(body):
 
 func _on_animation_player_animation_finished(anim_name):
 	rollingAnimActiveFrame = 0
+	pause_idle=false
+	pause_movement=false
