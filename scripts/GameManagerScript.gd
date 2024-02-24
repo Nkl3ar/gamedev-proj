ea8a1c48420c4dc64e667aa2a7408308b9072e2e
Filip Tutić <ftutic@vub.hr>
2024-02-24 21:49:13 +0100

    save time

--- scripts/GameManagerScript.gd
+++ scripts/GameManagerScript.gd
@@ -1,9 +1,36 @@
 extends Node
 class_name GameManager
 
+const SAVE_FILE = "user://time_table.sav"
+
 var musicVol = 100
 var sfxVol = 100
 var time: float = 0.0
+var beatTheGame = false
+
+var timeArray = [0.0,0.0,0.0]
+
+func sort_ascending(a, b):
+	if a == 0.0:
+		return true
+	if a < b:
+		return true
+	return false
+
+func save_time():
+	var file = FileAccess.open(SAVE_FILE, FileAccess.READ_WRITE)
+	if (FileAccess.file_exists(file)):
+		timeArray[0] = file.get_float()
+		timeArray[1] = file.get_float()
+		timeArray[2] = file.get_float()
+	timeArray.append(time)
+	timeArray.sort_custom(sort_ascending)
+	
+	file.store_float(timeArray[0])
+	file.store_float(timeArray[1])
+	file.store_float(timeArray[2])
+	
+	file.close()
 
 func level_restart():
 	get_tree().reload_current_scene()
