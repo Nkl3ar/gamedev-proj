04fe4724744100ec0994ec63adf2ea308c86d822
Filip Tutić <ftutic@vub.hr>
2024-02-24 22:54:19 +0100

    hud progress bars

--- scripts/HUD.gd
+++ scripts/HUD.gd
@@ -3,6 +3,14 @@ extends Node
 var hours: int = 0
 var minutes: int = 0
 var seconds: int = 0
+@export var sbLow: StyleBoxFlat
+@export var sbMed: StyleBoxFlat
+@export var sbHigh: StyleBoxFlat
+@export var sbOC: StyleBoxFlat
+func _ready():
+	$HealthBar.max_value = 5
+	$HealthBar.value = 5
+	$ChargeBar.value = 100
 
 func _process(delta):
 	GameManagerScript.time+=delta
@@ -15,7 +23,21 @@ func _process(delta):
 
 func _on_p_damageable_update_health(health):
 	$Health/HealthValue.text = str(health)
+	$HealthBar.value=health
+	if health<2:
+		$HealthBar.add_theme_stylebox_override("fill", sbLow)
+	elif health<5:
+		$HealthBar.add_theme_stylebox_override("fill", sbMed)
+	elif health<6:
+		$HealthBar.add_theme_stylebox_override("fill", sbHigh)
+	else:
+		$HealthBar.add_theme_stylebox_override("fill", sbOC)
 
 
 func _on_player_update_charge(charge):
 	$Charge/ChargeValue.text = str(charge) + "%"
+	$ChargeBar.value=charge
+	if charge<30:
+		$ChargeBar.add_theme_stylebox_override("fill", sbLow)
+	else:
+		$ChargeBar.add_theme_stylebox_override("fill", sbMed)
