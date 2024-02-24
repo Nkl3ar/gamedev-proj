a98b053f335d2ededc8175ddb633903122b545ed
Filip Tutić <ftutic@vub.hr>
2024-02-25 00:10:42 +0100

    hell

--- scripts/MeleeEnemyAI.gd
+++ scripts/MeleeEnemyAI.gd
@@ -22,7 +22,7 @@ func _physics_process(delta):
 			attacking = true
 			$Marker2D/Hurtbox.enable_damage()
 			$Marker2D/Attack.play()
-			velocity.x = direction * ATTACK_VELOCITY*50
+			velocity.x = direction * ATTACK_VELOCITY*30
 			move_and_slide()
 		else:
 			$Marker2D/Hurtbox.disable_damage()
